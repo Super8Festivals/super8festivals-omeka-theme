006c54e16f1ebb9fe3b387abc2f25e5242893a87
Michael Gates <michaelgatesdev@gmail.com>
2019-11-10 11:15:00 -0500

    Use partial for navbar

--- _partials/nav-top.php
+++ _partials/nav-top.php
@@ -1,50 +1,29 @@
-<div class="nav-top">
-    <div class="container-fluid">
-
-        <!--Notice & Search Row-->
-        <div class="row">
-            <div class="col vertical-align">
-                <!--Cartridge Notice-->
-                <div class="cartridge-notice">
-                    <p class="vertical-align">
-                        <span style="">*</span>
-                        <span style="">super 8<br>cartridge</span>
-                    </p>
-                </div>
-            </div>
-            <div class="col vertical-align">
-                <!-- Search -->
-                <div class="col vertical-align horizontal-align">
-                    <div class="input-group search">
-                        <form id="search-form" name="search-form" action="search" method="get">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="la la-search"></i></span>
-                            </div>
-                            <input name="query" id="query" value type="text" class="form-control" placeholder="<?php echo $searchPlaceholderText; ?>" aria-label="Search" aria-describedby="basic-addon1">
-                            <button name="submit_search" id="submit_search" type="submit" value="Search"><?php echo $searchButtonText; ?></button>
-                        </form>
+<!--Main Navigation-->
+<div class="container-fluid simple-nav-top">
+    <div class="row no-gutters vertical-align">
+        <!--Branding-->
+        <div class="col-lg-3">
+            <h1 class="nav-brand">
+                <a href="/">
+                    <img src="<?php echo src('logo.png', 'images/'); ?>" class="logo" alt="Super 8 Festivals">
+                </a>
+            </h1>
+        </div>
+        <!--Links-->
+        <div class="col-lg horizontal-align links">
+            <?php echo public_nav_main(); ?>
+        </div>
+        <!--Search-->
+        <div class="col-lg-3 horizontal-align vertical-align">
+            <div class="input-group search">
+                <form id="search-form" name="search-form" action="search" method="get">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"><i class="la la-search"></i></span>
                     </div>
-                </div>
+                    <input name="query" id="query" value type="text" class="form-control" placeholder="Enter search text here..." aria-label="Search" aria-describedby="basic-addon1">
+                    <button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
+                </form>
             </div>
         </div>
-
-        <!--Banner Row-->
-        <div class="row">
-            <div class="col banner-bg vertical-align horizontal-align">
-                <h1 class="nav-brand">
-                    <a href="/">
-                        <img src="<?php echo src('logo.png', 'images/'); ?>" class="logo" alt="Super 8 Festivals">
-                    </a>
-                </h1>
-            </div>
-        </div>
-
-        <!--Links Row-->
-        <div class="row">
-            <div class="col links vertical-align horizontal-align">
-                <?php echo public_nav_main(); ?>
-            </div>
-        </div>
-
     </div>
 </div>
--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $mapID = "map";
 $isPluginActive = plugin_is_active("SuperEightFestivals");
 ?>
 
+<?php echo $this->partial('_partials/nav-top.php'); ?>
+
 <!--Box-->
 <section class="super8-box" id="box" next="#world">
     <div class="container-fluid">
@@ -56,40 +58,7 @@ $isPluginActive = plugin_is_active("SuperEightFestivals");
             </div>
         </div>
     </div>
-
-
 </section>
-
-
-<!--Main Navigation-->
-<div class="container-fluid simple-nav-top">
-    <div class="row no-gutters vertical-align">
-        <!--Branding-->
-        <div class="col-lg-3">
-            <h1 class="nav-brand">
-                <a href="/">
-                    <img src="<?php echo src('logo.png', 'images/'); ?>" class="logo" alt="Super 8 Festivals">
-                </a>
-            </h1>
-        </div>
-        <!--Links-->
-        <div class="col-lg horizontal-align links">
-            <?php echo public_nav_main(); ?>
-        </div>
-        <!--Search-->
-        <div class="col-lg-3 horizontal-align vertical-align">
-            <div class="input-group search">
-                <form id="search-form" name="search-form" action="search" method="get">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text"><i class="la la-search"></i></span>
-                    </div>
-                    <input name="query" id="query" value type="text" class="form-control" placeholder="Enter search text here..." aria-label="Search" aria-describedby="basic-addon1">
-                    <button name="submit_search" id="submit_search" type="submit" value="Search">Search</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!--World (Map)-->
 <section class="container-fluid vertical-align" style="" id="world" next="#federation" previous="#box">
