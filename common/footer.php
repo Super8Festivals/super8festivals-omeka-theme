35103056b34fb3168f77fd52e8d8cf99ff04c29e
Michael Gates <michaelgatesdev@gmail.com>
2020-04-14 17:35:23 -0400

    Add proper footer

--- common/footer.php
+++ common/footer.php
@@ -1,10 +1,25 @@
 </main><!-- end wrap -->
 
-
-<footer role="contentinfo" class="footer mt-auto py-3">
-    <div class="container">
-        <div class="d-flex justify-content-center">
-            <p class="mb-0 text-muted">Copyright &copy; Super8Festivals 2019 All Rights Reserved.</p>
+<footer class="container pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+        <div class="col-12 col-md">
+            <img class="mb-2" src="<?php echo src('logo.png', 'images'); ?>" alt="" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2019-<?= date("Y") ?></small>
+        </div>
+        <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Federation</a></li>
+                <li><a class="text-muted" href="#">History</a></li>
+                <li><a class="text-muted" href="#">Filmmakers</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Contact</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Submit Content</a></li>
+                <li><a class="text-muted" href="#">Email Us</a></li>
+            </ul>
         </div>
     </div>
     <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
