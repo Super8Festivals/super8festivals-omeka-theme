a74e602837099ff00c0670bc0796dd99f3530fcb
Michael Gates <michaelgatesdev@gmail.com>
2020-04-14 17:41:51 -0400

    Move style to proper location, use shorthand for horizontal padding

--- index.php
+++ index.php
@@ -4,13 +4,60 @@ echo head(array(
 ));
 $isPluginActive = plugin_is_active("SuperEightFestivals");
 ?>
+<style>
+    .ol-popup {
+        position: absolute;
+        background-color: white;
+        filter: drop-shadow(0 1px 4px rgba(0, 0, 0, 0.2));
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #cccccc;
+        bottom: 12px;
+        left: -50px;
+        min-width: 300px;
+    }
 
+    .ol-popup:after, .ol-popup:before {
+        top: 100%;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+    }
+
+    .ol-popup:after {
+        border-top-color: white;
+        border-width: 10px;
+        left: 48px;
+        margin-left: -10px;
+    }
+
+    .ol-popup:before {
+        border-top-color: #cccccc;
+        border-width: 11px;
+        left: 48px;
+        margin-left: -11px;
+    }
+
+    .ol-popup-closer {
+        text-decoration: none;
+        position: absolute;
+        top: 2px;
+        right: 8px;
+    }
+
+    .ol-popup-closer:after {
+        content: "✖";
+    }
+</style>
 <section class="container-fluid" id="home-section">
 
     <!--2 Col Body-->
     <div class="row no-gutters">
         <!--Side Navigation-->
-        <div class="col-lg-4 pl-2 pr-2 d-flex flex-column">
+        <div class="col-lg-4 d-flex flex-column px-4">
             <a href="federation">
                 <img src="<?php echo src('FederationLogoFull.png', 'images'); ?>" class="img-fluid" alt="Federation">
             </a>
@@ -25,54 +72,6 @@ $isPluginActive = plugin_is_active("SuperEightFestivals");
         </div>
         <!--World Map-->
         <div class="col-lg-8 d-flex justify-content-center align-items-center flex-column" style="height: 70vh;">
-            <style>
-                .ol-popup {
-                    position: absolute;
-                    background-color: white;
-                    filter: drop-shadow(0 1px 4px rgba(0, 0, 0, 0.2));
-                    padding: 15px;
-                    border-radius: 10px;
-                    border: 1px solid #cccccc;
-                    bottom: 12px;
-                    left: -50px;
-                    min-width: 300px;
-                }
-
-                .ol-popup:after, .ol-popup:before {
-                    top: 100%;
-                    border: solid transparent;
-                    content: " ";
-                    height: 0;
-                    width: 0;
-                    position: absolute;
-                    pointer-events: none;
-                }
-
-                .ol-popup:after {
-                    border-top-color: white;
-                    border-width: 10px;
-                    left: 48px;
-                    margin-left: -10px;
-                }
-
-                .ol-popup:before {
-                    border-top-color: #cccccc;
-                    border-width: 11px;
-                    left: 48px;
-                    margin-left: -11px;
-                }
-
-                .ol-popup-closer {
-                    text-decoration: none;
-                    position: absolute;
-                    top: 2px;
-                    right: 8px;
-                }
-
-                .ol-popup-closer:after {
-                    content: "✖";
-                }
-            </style>
             <div id="popup" class="ol-popup d-flex align-items-center">
                 <a href="#" id="popup-closer" class="ol-popup-closer"></a>
                 <div id="popup-content" class="text-capitalize"></div>
